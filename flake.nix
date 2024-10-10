bf65ab48e011f77fd6154bfaf61b01f12d219245
Vaughn Coates <45183060+vonjackets@users.noreply.github.com>
2024-10-10 12:15:28 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix}:
+  outputs = { self, nixpkgs, flake-utils}:
     flake-utils.lib.eachDefaultSystem (system:
       let    
         pkgs = import nixpkgs { inherit system; };
