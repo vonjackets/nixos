ab7fe5ad5114bc4e0f7dd99ba4b6b439f1b78bad
Vaughn Coates <vcaaron@rocketmail.com>
2026-04-20 16:49:18 -0400

    fix packages common

--- hosts/common/packages.nix
+++ hosts/common/packages.nix
@@ -1,88 +1,77 @@
+{ pkgs, ... }:
 {
-  pkgs
-};
-# Define packages intended for use in the a typical developer environment
-# These will be shared across hosts as a common baseline set. Each host might add their own to this list, downstream.
-let
-    systemPkgs = with pkgs; [
-      # -- Basic Required Files --
-     gnugrep # GNU version of grep for searching text
-     gnused # GNU version of sed for text processing
-     gnutar # GNU version of tar for archiving
-     gzip # Compression utility
-
-
-     ghostty # a simply better terminal
-     nushell # a simply better shell
-     vim # THE editor of editors
-     zed-editor # because we actually code
-     starship
-     atuin
-
-     mask # a pretty good comamnd runner
-     # -- OpenSSL --
-     cacert
-     dropbear
-     openssh
-     openssl
-     openssl.dev
-
-     # -- Development tools --
-     podman-tui # status of containers in the terminal
-     docker-compose # starts group of containers for dev
-     zoxide # better than cd
-     minikube # local k8s testing
-     kubectl
-     fluxcd
-     bat
-     curl
-     xh
-     delta
-     direnv
-     eza
-     fd
-     findutils
-     fzf
-     gawk
-     getent
-     git
-     gnugrep
-     iproute2
-     jq
-     yq
-     lsof
-     man
-     man-db
-     man-pages
-     man-pages-posix
-     ncurses
-     procps
-     ps
-     ripgrep
-     rsync
-     rustlings
-     strace
-     tree
-     tree-sitter
-     which
-
-     # dhall - actually good configuration
-     dhall
-     dhall-yaml
-     dhall-json
-     dhall-lsp-server
-
-     # -- Compilers, Etc. --
-     cmake
-     gnumake
-     glibc
-     grc
-     pkg-config
-     util-linux
-     sops
-     envsubst
-    ];
-in
-{
-    inherit systemPkgs;
+  environment.systemPackages = with pkgs; [
+    # -- Basic Required Files --
+    gnugrep
+    gnused
+    gnutar
+    gzip
+    ghostty
+    nushell
+    vim
+    zed-editor
+    starship
+    atuin
+    mask
+    # -- OpenSSL --
+    cacert
+    dropbear
+    openssh
+    openssl
+    openssl.dev
+    # -- Development tools --
+    podman-tui
+    docker-compose
+    zoxide
+    minikube
+    kubectl
+    fluxcd
+    bat
+    curl
+    xh
+    delta
+    direnv
+    eza
+    fd
+    findutils
+    fzf
+    gawk
+    getent
+    git
+    iproute2
+    jq
+    yq
+    lsof
+    man
+    man-db
+    man-pages
+    man-pages-posix
+    ncurses
+    procps
+    ps
+    ripgrep
+    rsync
+    rustlings
+    strace
+    tree
+    tree-sitter
+    which
+    # -- Dhall --
+    dhall
+    dhall-yaml
+    dhall-json
+    dhall-lsp-server
+    # -- Compilers, Etc. --
+    cmake
+    gnumake
+    glibc
+    grc
+    pkg-config
+    util-linux
+    sops
+    envsubst
+    # -- Podman / container runtime deps --
+    slirp4netns
+    fuse-overlayfs
+  ];
 }
