fb2a765c649de0d32bab9b1d52e614e136d18585
Vaughn Coates <vcaaron@rocketmail.com>
2026-04-21 14:22:34 -0400

    manage zed via homemanager for all systems

--- hosts/common/home.nix
+++ hosts/common/home.nix
@@ -36,6 +36,46 @@ in
     package = pkgs.firefox;
   };
 
+  programs.zed-editor = {
+    enable = true;
+    extensions = [ "nix" "toml" "html" "python" ];
+    userSettings = {
+      telemetry = {
+        diagnostics = false;
+        metrics = false;
+      };
+      vim_mode = true;
+      ui_font_size = 16;
+      buffer_font_size = 16;
+      theme = {
+        mode = "system";
+        light = "Monokai Night";
+        dark = "One Dark";
+      };
+      terminal = {
+        shell = {
+          program = "nu";
+        };
+      };
+      load_direnv = "shell_hook";
+      auto_update = false;
+      lsp = {
+        rust-analyzer = {
+          binary = {
+            path_lookup = true;
+          };
+        };
+        nix = {
+          binary = {
+            path_lookup = true;
+          };
+        };
+      };
+    };
+  };
+
+  # Add our desired theem
+  home.file.".config/zed/themes/monokai-charcoal.json".source = ./programs/zed/themes/monokai-charcoal.json;
   # dotfile symlinks
   home.file.".config/git/config".source = ./programs/.gitconfig;
   # setup nushell stuff
